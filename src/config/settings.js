ad56a39c039cf94246a85e45951d113b2a31dad2
Ref-DevSa <ref.devsa@gmail.com>
2026-09-17 17:00:07 +0200

    Add configurable reservation settings

---
+++ src/config/settings.js
@@ -0,0 +1,9 @@
+const settings = {
+  seatsPerEvent: 20,
+  holdExpirySeconds: 60,
+  maximumActiveHoldsPerUser: 2,
+  maximumHoldsPerUserPerHour: 5,
+  maximumExtensionsPerHold: 2
+};
+
+module.exports = settings;
